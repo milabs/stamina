96be4f9d020184605a09c7eaa3b109251298c2fe
Ilya V. Matveychikov <matvejchikov@gmail.com>
2020-02-22 01:48:03 +0300

    Completely rewritten JS script, should be better

--- server/static/chart.js
+++ server/static/chart.js
@@ -1,102 +1,119 @@
-var draw = undefined
-var cells = new Object()
-var syscalls = new Object()
-const color_scale = chroma.scale('OrRd').domain([0, 16384])
+const THRAD_SIZE = 16384 // FIXME: detect automatically
 
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', url, true)
-    xhr.responseType = 'json'
-    xhr.onload = function() {
-	var status = xhr.status
-	if (status === 200) {
-	    callback(null, xhr.response)
-	} else {
-	    callback(status, xhr.response)
+var stam = {
+
+    ajax: function(url, callback) {
+	var xhr = new XMLHttpRequest()
+	xhr.open('GET', url, true)
+	xhr.responseType = 'json'
+	xhr.onload = function() {
+	    var status = xhr.status
+	    if (status === 200) {
+		callback(null, xhr.response)
+	    } else {
+		callback(status, xhr.response)
+	    }
 	}
-    }
-    xhr.send()
-}
+	xhr.send()
+    },
 
-var redrawCells = function() {
-    if (draw === undefined) {
-	draw = SVG().addTo('#canvas')
-	draw.on('mouseout', (e) => {
-	    let div = document.getElementById("status")
-	    div.style.visibility = 'hidden'
-	})
-
-	var keys = new Array()
-	Object.keys(cells).forEach(function(key) {
-	    if (cells.hasOwnProperty(key))
-		keys.push(key)
-	})
-
-	const cols = 32, size = 32, span = 2
-	const count = keys.sort((a, b) => a - b).length
-
-	for (var id = 0; id < count; id++) {
-	    let c = cells[id], col = id % cols, row = Math.floor(id / cols)
-	    let x = col * (size + span), y = row * (size + span)
-	    let g = draw.group()
-	    g.rect(size, size), g.text('#' + id)
-	    g.on('mouseover', (e) => {
-		let div = document.getElementById("status")
-		div.innerHTML = '<b>' + c.name + '</b><br>' + 'stack-min: ' + c.min.toString() + '<br>' + 'stack-max: ' + c.max.toString()
-		div.style.position = 'absolute'
-		div.style.top = e.y + 16 + 'px'
-		div.style.left = e.x + 16 + 'px'
-		div.style.visibility = 'visible'
-		div.style.background = chroma('yellow').hex()
+    init: function(timeout) {
+	let self = this
+	self.ajax('syscalls', (err, data) => {
+	    Object.keys(data).forEach((key) => {
+		self.sys_call_table[key] = {
+		    id: parseInt(key), name: data[key],
+		}
 	    })
-	    c.g = g.move(x, y)
-	}
+	    self.tick(timeout)
+	})
+    },
 
-	draw.size(cols * (size + span) - span, Math.ceil(count / cols) * (size + span) - span)
-    }
+    tick: function(timeout) {
+	let self = this
+	self.ajax('stam', (err, data) => {
+	    self.show(data), setTimeout(() => {
+		self.tick(timeout)
+	    }, timeout)
+	})
+    },
 
-    Object.keys(cells).forEach(function(id) {
-	let c = cells[id]
-	if (!c.dirty) return
-	else if (c.name == 'n/a' && (!c.min && !c.max && !c.hit)) {
-	    c.g.children()[0].fill(chroma('lightgrey').hex())
-	    c.g.children()[1].text('#' + id + '\nN/A\n')
-	} else {
-	    c.g.children()[0].fill(color_scale(c.max).hex())
-	    c.g.children()[1].text('#' + id + '\n' + c.hit + '\n')
-	}
-	c.dirty = undefined
-    })
-}
-
-var updateCells = function() {
-    getJSON("stam", function(err, data) {
+    show: function(data) {
+	let self = this
+	self.draw(data)
 	data.forEach((item, id) => {
-	    if (cells[id] === undefined) {
-		let name = (syscalls[id] == undefined) ? 'n/a' : syscalls[id]
-		cells[id] = {
-		    id: id.toString(), name: name,
+	    let t = self.sys_call_table[id]
+	    if (t.hit != item.Hit || t.min != item.Min || t.max != item.Max) {
+		t.hit = item.Hit, t.min = item.Min, t.max = item.Max
+		if (t.notimplemented && (!t.hit && !t.min && !t.max)) {
+		    t.g.children()[0].fill(chroma('lightgray').hex())
+		    t.g.children()[1].text('#' + id + '\n' + t.name + '\n')
+		} else {
+		    t.g.children()[0].fill(self.scale(t.max).hex())
+		    t.g.children()[1].text('#' + id + '\n' + t.hit + '\n')
 		}
 	    }
-	    let c = cells[id]
-	    if (c.hit != item.Hit || c.min != item.Min || c.max != item.Max) {
-		c.hit = item.Hit
-		c.min = item.Min
-		c.max = item.Max
-		c.dirty = 1
-	    }
 	})
-	redrawCells()
-    })
+    },
+
+    draw: function(data) {
+	let self = this
+	if (self.map === undefined) {
+	    const cols = 32
+	    const rows = Math.ceil(data.length / cols)
+	    const span = 2, size = 40
+
+	    self.map = SVG().addTo('#map')
+	    self.map.on('mouseout', (e) => {
+		let div = document.getElementById("map-status")
+		div.style.visibility = 'hidden'
+	    })
+
+	    data.forEach((item, id) => {
+		if (self.sys_call_table[id] === undefined) {
+		    self.sys_call_table[id] = {
+			id: parseInt(id), name: 'N/A', notimplemented: 1,
+		    }
+		}
+		let d = self.sys_call_table[id]
+		d.g = self.map.group()
+		d.g.rect(size, size)
+		d.g.text(d.name).font({ family: 'Inconsolata', size: 12 })
+		d.g.on('mouseover', (e) => {
+		    let div = document.getElementById("map-status")
+		    div.innerHTML = '<b>' + d.name + '</b> [' + d.min + '/' + d.max + ']'
+		    div.style.position = 'absolute'
+		    div.style.top = e.y + 16 + 'px'
+		    div.style.left = e.x + 16 + 'px'
+		    div.style.visibility = 'visible'
+		    div.style.fontFamily = 'Inconsolata'
+		    div.style.background = chroma('yellow').hex()
+		})
+	    })
+	    
+	    data.forEach((item, id) => {
+		const c = id % cols, r = Math.floor(id / cols)
+ 		const x = c * (size + span), y = r * (size + span)
+		self.sys_call_table[id].g.move(x, y)
+	    })
+
+	    self.map.size(cols * size + (cols + 1) * span,
+			  rows * size + (rows + 1) * span)
+	}
+	if (self.top === undefined) {
+	    self.top = document.getElementById("top")
+	}
+    },
+
+    map: undefined,
+    top: undefined,
+
+    sys_call_table: {},
+    scale: chroma.scale('OrRd').domain([0, THRAD_SIZE]),
+    topHit: [], topMax: [],
+
 }
 
 SVG.on(document, 'DOMContentLoaded', function() {
-    getJSON("syscalls", function(err, data) {
-	Object.keys(data).forEach(function(key) {
-	    syscalls[key] = data[key]
-	})
-	setInterval(function () {
-	    updateCells()
-	}, 250)
-    })
+    stam.init(250)
 })
